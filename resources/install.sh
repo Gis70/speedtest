32c420d7f8b62481f80e338ca251386ea1d66c9b
Ghislain <ghislainchouet@gmail.com>
2022-04-23 19:26:09 +0200

    ajout speedtest security.conf

--- resources/install.sh
+++ resources/install.sh
@@ -11,6 +11,9 @@ echo "***** Commande: install python-pip**********"
 sudo apt-get install -y python-pip
 echo "***** Commande: install speedtest-cli **********"
 pip install git+https://github.com/sivel/speedtest-cli.git
+echo "***** Commande: ajout domaine SpeedTest dans le security.conf d'Apache **********"
+sudo sed -i "s/img-src 'self'/& *.speedtest.net/" /etc/apache2/conf-available/security.conf
+sudo systemctl restart apache2
 echo 100 > ${PROGRESS_FILE}
 echo "********************************************************"
 echo "*             Installation terminée                    *"
